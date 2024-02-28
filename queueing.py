a5ce3b43782719ba0f526f4e83d528b97c2e9072
dchou1618 <dvchou@andrew.cmu.edu>
2024-02-28 14:28:32 -0800

    preliminary files
    vpm, queueing experiment (1 out of the 4 use cases)

---
+++ queueing.py
@@ -0,0 +1,7 @@
+
+
+
+'''
+Implement sampling from the transitions of Geo/Geo/1/queue, specified in section D page 17 of the paper.
+Serfozo 2009 for details on the closed form stationary distribution of discrete M/M/1 queue.
+'''
---
+++ vpm.py
@@ -0,0 +1,7 @@
+import torch
+import numpy as np
+
+'''
+Implement variational power method based on paper
+'''
+
