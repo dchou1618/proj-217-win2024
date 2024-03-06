7f7080e607e036dbc977aa9ad298783692d9952d
dchou1618 <dvchou@andrew.cmu.edu>
2024-03-06 01:30:50 -0800

    queueing and figures
    figures generated from seaborn added to queueing file. debugging the process of estimating stationary and ensuring correctness with tabular model-based approach..

--- queueing.py
+++ queueing.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy.stats
+import seaborn as sns
+import pandas as pd
 
 '''
 (1) Implement tabular model method by building up the transition matrix directly using the 
@@ -67,19 +69,79 @@ def sample_nsteps(n, initial_state, qa, qf, transition_func=gg1_step):
 def ground_truth_queueing_stationary(qa, qf, max_state):
 	rho = (qa*(1-qf))/(qf*(1-qa))
 	return [(1-rho)*(rho**i) for i in range(0, max_state+1)]
+
+'''
+(5) Plot generation
+'''
+
+def generate_plots(qa, qf, min_val, max_val, increment, var_name, runs, plot_name):
+	log_kl_dict = {"method":[], var_name: [], 
+                  "log-kl-div": []}
+	for transition_samples in range(min_val, max_val+1, increment):
+		num_runs = 0
+		while num_runs < runs:
+			state_seq, max_state = sample_nsteps(n=transition_samples, initial_state=0, qa=qa, qf=qf)
+			tabular_model = build_tabular(state_seq, max_state)
+			
+			estimated = estimate_tabular_stationary(tabular_model, iters=1000)
+			# true distribution only up to a maximum state.
+			true = ground_truth_queueing_stationary(qa, qf, max_state)
+			log_kl = np.log(scipy.stats.entropy(estimated, true))
+			if (log_kl != float("inf")):
+				num_runs+=1
+
+				log_kl_dict["method"].append("model-based")
+				log_kl_dict["transition_samples"].append(transition_samples)
+				log_kl_dict["log-kl-div"].append(log_kl)
+	log_kl_pd = pd.DataFrame(log_kl_dict)
+	log_kl_plot = sns.lineplot(data=log_kl_pd, x="transition_samples", y="log-kl-div", hue="method", marker="o")
+	log_kl_plot.set(ylim=(-5, 0))
+	fig = log_kl_plot.get_figure()
+	fig.savefig(f"{plot_name}.png") 
 	
+def generate_plots_over_qf(qa, min_val, max_val,  runs, plot_name):
+	log_kl_dict = {"method":[], "qf": [], 
+	"log-kl-div":[]}
+	qfs = [np.round(min_val+(0.01)*i, 2) for i in range(int((max_val-min_val)/0.01 )+2)]
+
+	for qf in qfs:
+		num_runs = 0
+		while num_runs < runs:
+			state_seq, max_state = sample_nsteps(n=200, initial_state=0, qa=qa, qf=qf)
+			tabular_model = build_tabular(state_seq, max_state)
+			print(tabular_model)
+			if (np.linalg.matrix_rank(tabular_model) != tabular_model.shape[0]):
+				continue
+			estimated = estimate_tabular_stationary(tabular_model, iters=1000)
+			true = ground_truth_queueing_stationary(qa, qf, max_state)
+			log_kl = np.log(scipy.stats.entropy(estimated, true))
+			if (log_kl != float("inf")):
+				num_runs+=1
+				log_kl_dict["method"].append("model-based")
+				log_kl_dict["qf"].append(qf)
+				log_kl_dict["log-kl-div"].append(log_kl)
+	log_kl_pd = pd.DataFrame(log_kl_dict)
+	log_kl_plot = sns.lineplot(data=log_kl_pd, x="qf", y="log-kl-div", hue="method", marker="o")
+	log_kl_plot.set(ylim=(-5, 0))
+	fig = log_kl_plot.get_figure()
+	fig.savefig(f"{plot_name}.png")
+
 if __name__ == "__main__":
 	qa, qf = 0.8, 0.9
-	n_samples = 100
-	log_kl_sum = 0
+	min_transitions, max_transitions = 100, 500
 	runs = 10
-	for i in range(runs):
-		state_seq, max_state = sample_nsteps(n=n_samples, initial_state=0, qa=qa, qf=qf)
-		tabular_model = build_tabular(state_seq, max_state)
-		estimated = estimate_tabular_stationary(tabular_model, iters=1000)
-		# true distribution only up to a maximum state.
-		true = ground_truth_queueing_stationary(qa, qf, max_state)
-		log_kl_sum += np.log(scipy.stats.entropy(estimated, true))
-	print(f"Log-kl-divergence for {n_samples}: {log_kl_sum/runs}")
+	
+	state_seq, max_state = sample_nsteps(n=200, initial_state=0, qa=qa, qf=qf)
+	tabular_model = build_tabular(state_seq, max_state)
+	estimated = estimate_tabular_stationary(tabular_model, iters=1000)
+	# true distribution only up to a maximum state.
+	true = ground_truth_queueing_stationary(qa, qf, max_state)
+	print(estimated)
+	print(true)
+	log_kl = np.log(scipy.stats.entropy(estimated, true))
 
-
+	print(log_kl)
+	
+	#generate_plots(qa, qf, min_transitions, max_transitions, 100, "transition_samples", runs, "figure2")
+	#generate_plots_over_qf(qa, 0.82, 0.90, runs, "figure2_qf")
+	
