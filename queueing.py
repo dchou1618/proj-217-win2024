3f16c9402e60146dbc7bdaebcab9b7635f54a45c
dchou1618 <dvchou@andrew.cmu.edu>
2024-03-07 19:42:51 -0800

    working through vpm approach
    vpm approach based on paper using pytorch

--- queueing.py
+++ queueing.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.stats
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
 
 '''
 (1) Implement tabular model method by building up the transition matrix directly using the 
@@ -95,21 +96,26 @@ def generate_plots(qa, qf, min_val, max_val, increment, var_name, runs, plot_nam
 				log_kl_dict["log-kl-div"].append(log_kl)
 
 	log_kl_pd = pd.DataFrame(log_kl_dict)
-	log_kl_plot = sns.lineplot(data=log_kl_pd, x="transition_samples", y="log-kl-div", hue="method", marker="o", palette=["darkblue"])
+	log_kl_plot = sns.lineplot(data=log_kl_pd, x="transition_samples", y="log-kl-div", hue="method", marker="o", palette=["darkblue"], markersize=8)
 	log_kl_plot.set(ylim=(-5, 0))
+	plt.grid()
 	fig = log_kl_plot.get_figure()
 	fig.savefig(f"{plot_name}.png") 
 	
 def generate_plots_over_qf(qa, min_val, max_val,  runs, plot_name):
 	log_kl_dict = {"method":[], "qf": [], 
 	"log-kl-div":[]}
-	qfs = [np.round(min_val+(0.01)*i, 2) for i in range(int((max_val-min_val)/0.01 )+2)]
+	qfs = [np.round(min_val+(0.01)*i, 2) for i in range(int((max_val-min_val)/0.01 )+1)]
 
 	for qf in qfs:
+		print(qf)
 		num_runs = 0
 		while num_runs < runs:
+			
 			state_seq, max_state = sample_nsteps(n=200, initial_state=0, qa=qa, qf=qf)
 			tabular_model = build_tabular(state_seq, max_state)
+			if (np.isnan(tabular_model).any()):
+				continue
 			
 			estimated = estimate_tabular_stationary(tabular_model, iters=200)
 			true = ground_truth_queueing_stationary(qa, qf, max_state)
@@ -128,7 +134,7 @@ def generate_plots_over_qf(qa, min_val, max_val,  runs, plot_name):
 if __name__ == "__main__":
 	qa, qf = 0.8, 0.9
 	min_transitions, max_transitions = 100, 500
-	runs = 10
+	runs = 30
 	'''	
 	state_seq, max_state = sample_nsteps(n=100, initial_state=0, qa=qa, qf=qf)
 	tabular_model = build_tabular(state_seq, max_state)
@@ -142,6 +148,7 @@ if __name__ == "__main__":
 
 	print(log_kl)
 	'''
-	generate_plots(qa, qf, min_transitions, max_transitions, 100, "transition_samples", runs, "figure2")
-	# generate_plots_over_qf(qa, 0.82, 0.90, runs, "figure2_qf")
+	np.random.seed(42)
+	# generate_plots(qa, qf, min_transitions, max_transitions, 100, "transition_samples", runs, "figure2")
+	generate_plots_over_qf(qa, 0.82, 0.90, runs, "figure2_qf")
 	
--- vpm.py
+++ vpm.py
@@ -1,18 +1,19 @@
 import torch
 from torch import nn
+from torch.utils.data import DataLoader, TensorDataset
 import numpy as np
+import queueing
 
 '''
 (1) Implement variational power method based on paper
 '''
 
 class Tau(nn.Module):
-	def __init__(self, total_elems):
-		super.__init__()
-		self.flatten = nn.Flatten()
+	def __init__(self, total_states):
+		super().__init__()
 		# 4 hidden layers
 		self.model = nn.Sequential(
-			nn.Linear(total_elems, 128),
+			nn.Linear(total_states, 128),
 			nn.ReLU(),
 			nn.Linear(128, 128),
 			nn.ReLU(),
@@ -21,19 +22,73 @@ class Tau(nn.Module):
             nn.Linear(128, 128),
 			nn.ReLU(),
             nn.Linear(128, 128),
-			nn.ReLU(),
-            nn.Linear(128, 1),
-			nn.Softplus()
+			nn.ReLU()
 		)
+		self.output_layer = nn.Sequential(nn.Linear(128, 1),
+										  nn.Softplus())
 	def forward(self, x):
-		return self.model(x)
+		x = self.model(x)
+		x = self.output_layer(x)
+		return x
 
 
-def iterative_vpm(transition_data, alpha_theta, alpha_v, power_steps, M, B):
+def compute_gradients():
+	pass
+
+
+def iterative_vpm(D, alpha_theta, alpha_v, power_steps, M, B, _lambda, max_state, beta1):
+	tau = Tau(total_states = max_state + 1)
+	# reference network
+	tau_t = Tau(total_states = max_state + 1)
+	# update and fix reference network
+	tau_t.load_state_dict(tau.state_dict())
+	v = torch.rand(1, requires_grad=True)	
+	optim_theta = torch.optim.Adam(tau.parameters(), lr = alpha_theta, betas=(beta1, 0.999))
+	optim_v = torch.optim.Adam([v], lr = alpha_v)
+
 	for t in range(power_steps):
 		for m in range(M):
 			# fit transition data
-			pass
+			x = []
+			x_star = []
+			transition_batch = np.random.choice(len(D), size=B, replace=False)
+			sampled_transitions = D[transition_batch]
+			x = sampled_transitions[:,0]
+			# next state
+			x_star = sampled_transitions[:,1]
+			data_loader = DataLoader(TensorDataset(x, x_star), batch_size=B, shuffle=True)
+			for x, x_star in data_loader:
+				output = tau(x)
+				output.retain_grad()
+				optim_theta.zero_grad()
+				grad_theta, grad_v = compute_gradients(output, v, lambda_val)
+			print(x_star)
+			break
+		# update and fix reference network
+		tau_t = tau_theta.clone().detach()
+	return tau
+
+def get_D(n_steps, initial_state, qa, qf):
+	state_seq, max_state = queueing.sample_nsteps(n=n_steps+1, initial_state=initial_state,qa=qa,qf=qf)
+	transitions = []
+	for i in range(1,len(state_seq)):
+		transitions.append((state_seq[i-1], state_seq[i]))
+	return transitions, max_state
 
 if __name__ == "__main__":
-	pass
+	_lambda = 1
+	qa = 0.8
+	qf = 0.9
+	M = 10
+	alpha_theta = 0.0005
+	alpha_v = 0.0005
+	beta1 = 0.5
+	T = 1000
+	rho = (qa*(1-qf))/(qf*(1-qa))
+	B = int(np.ceil(40*rho))
+	D, max_state = get_D(100, 0, qa, qf)
+	D = np.array(D)
+	iterative_vpm(D, alpha_theta, alpha_v, T, M, B, _lambda, max_state, beta1)
+
+
+
