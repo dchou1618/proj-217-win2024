6d0373d60c010a9fd12ce0e88c33c8f70f630eea
dchou1618 <dvchou@andrew.cmu.edu>
2024-03-09 12:21:28 -0800

    added non iid sampling from queueing process
    non iid sampling yields log kl values closer to paper than iid sampling that they state was used in their paper.

--- queueing.py
+++ queueing.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 import vpm
-import estimate_stationary_dist as esd
+# import estimate_stationary_dist as esd
 
 '''
 (1) Implement tabular model method by building up the transition matrix directly using the 
@@ -74,6 +74,18 @@ def sample_nsteps(n, qa, qf, transition_func=gg1_step):
 		max_state = max(max(next_state, curr_state), max_state)
 	return state_seq, max_state
 
+
+def sample_nsteps_non_iid(n, initial_state, qa, qf, transition_func=gg1_step):
+	curr_state = initial_state
+	state_seq = []
+	max_state = initial_state
+	for _ in range(n):
+		next_state = transition_func(curr_state, qa, qf)
+		state_seq.append( (curr_state, next_state) )
+		max_state = max(curr_state, max(next_state, max_state))
+		curr_state = next_state
+	return state_seq, max_state
+
 '''
 (4) Ground truth stationary distribution
 '''
@@ -86,7 +98,7 @@ def ground_truth_queueing_stationary(qa, qf, max_state):
 (5) Plot generation
 '''
 
-def generate_plots(qa, qf, min_val, max_val, increment, var_name, runs, plot_name):
+def generate_plots(qa, qf, min_val, max_val, increment, var_name, runs, plot_name, run_biased=False):
 	_lambda = 1
 	M = 20
 	alpha_theta = 0.01
@@ -101,11 +113,14 @@ def generate_plots(qa, qf, min_val, max_val, increment, var_name, runs, plot_nam
 		print(f"Generating runs for transitions samples = {transition_samples}")
 		num_runs = 0
 		while num_runs < runs:
-			state_seq, max_state = sample_nsteps(n=transition_samples, qa=qa, qf=qf)
+			if run_biased:
+				state_seq, max_state = sample_nsteps_non_iid(transition_samples, 0, qa, qf)
+			else:
+				state_seq, max_state = sample_nsteps(n=transition_samples, qa=qa, qf=qf)
 			D = np.array(state_seq)
-			# tau = vpm.iterative_vpm(D, alpha_theta, alpha_v, T, M, B, _lambda, max_state, beta1)
-			# estimated_vpm = vpm.get_density(tau, max_state)
-			estimated_vpm = esd.estimate_stationary(D, max_state)
+			tau = vpm.iterative_vpm(D, alpha_theta, alpha_v, T, M, B, _lambda, max_state, beta1)
+			estimated_vpm = vpm.get_density(tau, max_state)
+			# estimated_vpm = esd.estimate_stationary(D, max_state)
 
 			tabular_model = build_tabular(state_seq, max_state)
 			estimated = estimate_tabular_stationary(tabular_model, iters=200)
@@ -130,9 +145,9 @@ def generate_plots(qa, qf, min_val, max_val, increment, var_name, runs, plot_nam
 	log_kl_plot.set(ylim=(-4, 5))
 	plt.grid()
 	fig = log_kl_plot.get_figure()
-	fig.savefig(f"{plot_name}.png") 
+	fig.savefig(f"{plot_name}"+("_non_iid" if run_biased else "")+".png") 
 	
-def generate_plots_over_qf(qa, min_val, max_val,  runs, plot_name):
+def generate_plots_over_qf(qa, min_val, max_val,  runs, plot_name, run_biased=False):
 	log_kl_dict = {"method":[], "Prob": [], "Log KL":[]}
 	qfs = [np.round(min_val+(0.01)*i, 2) for i in range(int((max_val-min_val)/0.01 )+1)]
 	_lambda = 1
@@ -148,7 +163,10 @@ def generate_plots_over_qf(qa, min_val, max_val,  runs, plot_name):
 		B = int(np.ceil(40*rho))
 		num_runs = 0
 		while num_runs < runs:
-			state_seq, max_state = sample_nsteps(n=200, qa=qa, qf=qf)
+			if run_biased:
+				state_seq, max_state = sample_nsteps_non_iid(200, 0, qa, qf)
+			else:
+				state_seq, max_state = sample_nsteps(n=200, qa=qa, qf=qf)
 			tabular_model = build_tabular(state_seq, max_state)
 			if (np.isnan(tabular_model).any()):
 				continue
@@ -180,22 +198,11 @@ def generate_plots_over_qf(qa, min_val, max_val,  runs, plot_name):
 	log_kl_plot.set(ylim=(-4, 5))
 	plt.grid()
 	fig = log_kl_plot.get_figure()
-	fig.savefig(f"{plot_name}.png")
+	fig.savefig(f"{plot_name}"+("_non_iid" if run_biased else "")+".png")
 
 if __name__ == "__main__":
-	# _lambda = 1
 	qa = 0.8
 	qf = 0.9
-	# M = 10
-	# alpha_theta = 0.01
-	# alpha_v = 0.01
-	# beta1 = 0.5
-	# T = 100
-	# rho = (qa*(1-qf))/(qf*(1-qa))
-	# B = int(np.ceil(40*rho))
-	# D, max_state = sample_nsteps(100, qa, qf)
-	# D = np.array(D)
-	# tau = iterative_vpm(D, alpha_theta, alpha_v, T, M, B, _lambda, max_state, beta1)
 
 	min_transitions, max_transitions = 100, 500
 	runs = 10
@@ -214,7 +221,9 @@ if __name__ == "__main__":
 	print(log_kl)
 	"""
 
-	np.random.seed(43)
+	np.random.seed(101)
 	generate_plots(qa, qf, min_transitions, max_transitions, 100, "transition_samples", runs, "figure2")
-	# generate_plots_over_qf(qa, 0.82, 0.90, runs, "figure2_qf")
+	generate_plots_over_qf(qa, 0.82, 0.90, runs, "figure2_qf")
+	generate_plots(qa, qf, min_transitions, max_transitions, 100, "transition_samples", runs, "figure2", run_biased=True)
+	generate_plots_over_qf(qa, 0.82, 0.90, runs, "figure2_qf", run_biased=True)
 	
